d0b86a333198cd351bc91c4699d34e90e87f93f1
Wendy Gwo <wgwo@hotmail.com>
2015-01-29 13:37:45 -0800

    Move changes between x and o because hooked up numMoves counter. Create database object to track x and o moves

--- js/ttt-logic.js
+++ js/ttt-logic.js
@@ -2,26 +2,53 @@ var tttApp = angular.module('tttApp',['firebase']);
 
 tttApp.controller('tttController', function($scope,$firebase){
 
+	//Set up the board in firebase
 	var ref=new Firebase('https://wendytictactoe.firebaseio.com/board');
 	//Create AngularFire reference to data
 	var sync = $firebase(ref);
 	//download the data into an array
 	$scope.board=sync.$asArray();
 
+	//Set up the number of moves counter in Firebase
 	var counterRef=new Firebase('https://wendytictactoe.firebaseio.com/counter');
 	//Create AngularFire reference to data
 	var counterSync = $firebase(counterRef);
 	//download the data into an array
 	$scope.counter=counterSync.$asArray();
 
-	//initialize board to empty strings
-	//$scope.board=['','','','','','','','',''] //old way of initializing the board
+	//Set up the array of x moves and y moves in Firebase 
+	//x moves is the first record under playerMoves
+	//0 moves is the second record under playerMoves
+	var playerMovesRef = new Firebase('https://wendytictactoe.firebaseio.com/playerMoves');
+	var playerMovesSync = $firebase(playerMovesRef);
+	$scope.playerMoves = playerMovesSync.$asArray();
+
+	$scope.playerMoves.$loaded(function(){
+		console.log('went into player moves. Length is:')
+		console.log($scope.playerMoves.length);
+		if ($scope.playerMoves.length==0){
+			$scope.playerMoves.$add({moveRecord: ['-1']});
+			$scope.playerMoves.$add({moveRecord: ['-2']});
+		}
+		else {
+			$scope.playerMoves[0].moveRecord=['-1'];
+			$scope.playerMoves[1].moveRecord=['-2'];
+			$scope.playerMoves.$save(0);
+			$scope.playerMoves.$save(1);
+
+		}
+	});
+
+
+	// });
+	// $scope.xMoves=[];
+	// $scope.oMoves=[];
 	
-	//the stuff here only happens after $scope.board is downloaded
-	//Will generate board if it doesn't already exist.
+	//The code below only executes after $scope.board is downloaded
+	//Will create board if it doesn't already exist.
 	//Otherwise it'll reset all the values to blanks
 	$scope.board.$loaded(function(){
-		console.log('Board length: ' + $scope.board.length);
+		// console.log('Board length: ' + $scope.board.length);
 		if ($scope.board.length==0){ //if board doesn't exist in current instance, generates it
 			for (var i = 0; i<9; i++){
 				$scope.board.$add({playerMove: ''});
@@ -34,49 +61,39 @@ tttApp.controller('tttController', function($scope,$firebase){
 		}
 	});
 
+	//The code below only executes after $scope.counter is downloaded
+	//Will create counter if it doesn't already exist and initialize it to 0
+	//Otherwise it'll reset the counter value to 0
 	$scope.counter.$loaded(function(){
-		console.log('Counter length: ' +$scope.counter.length);
-		if ($scope.counter.length==0){ //creates counter variable if it doesn't already exist
+		// console.log('Counter length: ' +$scope.counter.length);
+		if ($scope.counter.length==0){ 			//creates counter variable if it doesn't already exist
 			$scope.counter.$add({numMoves: 0})
-			console.log('Went into if scope counter length = 0.');
-		}else{ //updates counter variable if it already exists in database
+			// console.log('Went into if scope counter length = 0.');
+		}else{ 									//updates counter variable if it already exists in database
 			$scope.counter[0].numMoves=0;
 			$scope.counter.$save(0); //saves first element in counter
 		}
-
 	});
 
 	$scope.makeMove=function(idx){
-		console.log('going into makeMove function: ');
-		console.log('square clicked(index): '+idx)
-		$scope.board[idx].playerMove='x';
-		$scope.board.$save($scope.board[idx]);
-		//console.log(' Current scope counter = ' + $scope.counter[0].numMoves);
-		$scope.counter[0].numMoves++;
-		$scope.counter.$save(0); //saves first element in counter
-		console.log('Counter incremented in makeMove function. numMoves counter = ' + $scope.counter[0].numMoves);
+		console.log('Square number clicked(index): '+idx)
+		console.log('Current move number = ' + $scope.counter[0].numMoves);
+		console.log('Value in current box clicked: '+$scope.board[idx].playerMove);
+		//only allows move if board piece not already taken
+		if (($scope.board[idx].playerMove !='x') && ($scope.board[idx].playerMove!='o')){
+			console.log('Allowing player to move because current board position is empty');
+			if (($scope.counter[0].numMoves)%2 == 0){
+				$scope.board[idx].playerMove='x';
+				$scope.board.$save($scope.board[idx]);
+			} else {
+				$scope.board[idx].playerMove='o';
+				$scope.board.$save($scope.board[idx]);
+			}
+			$scope.counter[0].numMoves++;
+			$scope.counter.$save(0); //saves first element in counter
+			//console.log('Counter incremented in makeMove function. numMoves counter = ' + $scope.counter[0].numMoves);
+		}
 	}
 
 
-	// //Initialize turnNumber to 0
-	// $scope.turnNumber = 0;
-	// //Arrays to store x and o moves
-	// $scope.xMoves=[];
-	// $scope.oMoves=[];
-
-	// //Function to put X or O into the squares
-	// $scope.makeMove=function(idx){
-	// 	if (($scope.board[idx]!='x') && ($scope.board[idx]!='o')){ //This lines checks to see if the board piece has already been taken
-	// 		if ($scope.turnNumber%2==0){
-	// 			$scope.board[idx]='x';
-	// 			$scope.xMoves.push((idx+1).toString()); //pushes current piece position into an array. Adding 1 because index starts at zero. My win logic starts numbering the squares at 1
-	// 			console.log($scope.xMoves);
-	// 		} else {
-	// 			$scope.board[idx]='o';
-	// 			$scope.oMoves.push((idx+1).toString());//pushes current piece position into an array. Adding 1 because index starts at zero. My win logic starts numbering the squares at 1
-	// 			console.log($scope.oMoves);
-	// 		}
-	// 		$scope.turnNumber++;
-	// 	}
-	// }
 });
